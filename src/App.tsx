a21b44718a696a87cdace8113bf776e6d64c3f68
Umang Bhardwaj <umangbhardwaj0006@gmail.com>
2025-04-28 02:26:44 +0530

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
 	return (
     <div>
       <h1 className="text-7xl font-bold">Hello world!</h1>
+      <p className="text-6xl font-bold">Hello world!</p>
     </div>
 	);
 }
---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+
+const router = createBrowserRouter([{ index: true, Component: App }]);
+
+export default router;
